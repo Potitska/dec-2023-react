9c49160fd9b8087626f4eeedb9f07da1ab6620d9
Potitska <natal9a.poticka@icloud.com>
2023-04-26 19:22:12 +0300

    homeWork4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {Route, Routes} from "react-router-dom";
+import CommentsPage from "./pages/CommentsPage/CommentsPage";
+import AlbumsPage from "./pages/AlbumsPage/AlbumsPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path={'/'} element={'<MainLayout/>'}>
+        <Route path={'todos'} element={<TodosPage/>}/>
+        <Route path={'albums'} element={<AlbumsPage/>}/>
+        <Route path={'comments'} element={<CommentsPage/>}/>
+
+      </Route>
+      <Routes/>
+  )
 }
 
 export default App;
