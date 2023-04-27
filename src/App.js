1d88e0c9d530e61475d728a2de2d0ed9389dd2bb
Potitska <natal9a.poticka@icloud.com>
2023-04-27 20:45:02 +0300

    hw5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {Navigate, Route, Routes} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import PostPage from "./pages/PostPage";
+import CommentPage from "./pages/CommentPage";
+import CatDogPage from "./pages/CatDogPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path={'/'} element={<MainLayout/>}>
+            <Route index element={<Navigate to={'posts'}/>}/>
+            <Route path={'posts'} element={<PostPage/>}/>
+            <Route path={'comments'} element={<CommentPage/>}/>
+            <Route path={'cats_dogs'} element={<CatDogPage/>}/>
+       </Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/CatDogPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CatDogPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CatDogPage;
